a38b53cec3455dee59e3a88bb1c277e968dfb82e
Anthony Erlinger <aerlinger@gmail.com>
2016-07-05 21:38:29 -0400

    Add basic layout

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,6 +5,7 @@ import styles from './Home.css';
 import AceEditor from 'react-ace';
 import MarkdownPreview from './MarkdownPreview'
 import MDReactComponent from 'markdown-react-js'
+import ReactGridLayout from 'react-grid-layout'
 
 import 'brace/mode/python';
 import 'brace/theme/github';
@@ -17,16 +18,20 @@ export default class Home extends Component {
   render() {
     return (
       <div id="main">
-        <AceEditor
-          mode="python"
-          theme="github"
-          onChange={onChange}
-          name="ace_editor"
-          editorProps={
-          { $blockScrolling: true }
-        }
-        />
-        <MarkdownPreview mdtext="Some text with **emphasis!**"/>
+        <ReactGridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+          <div key="a" _grid={{x: 0, y: 0, w: 6, h: 2}}>
+            <AceEditor
+              mode="python"
+              theme="github"
+              onChange={onChange}
+              name="ace_editor"
+              editorProps={{ $blockScrolling: true }}
+            />
+          </div>
+          <div key="b" _grid={{x: 6, y: 0, w: 6, h: 2}}>
+            <MarkdownPreview mdtext="Some text with **emphasis!**"/>
+          </div>
+        </ReactGridLayout>
       </div>
     );
   }
