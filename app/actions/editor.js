8ce909caa9731508cef04d2cf14891b074dc6022
Anthony Erlinger <aerlinger@gmail.com>
2016-07-05 21:26:40 -0400

    Add basic markdown rendering

---
+++ app/actions/editor.js
@@ -0,0 +1,5 @@
+export function updateText(text) {
+  return {
+
+  }
+}
--- app/components/Home.js
+++ app/components/Home.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router';
 import styles from './Home.css';
 
 import AceEditor from 'react-ace';
+import MarkdownPreview from './MarkdownPreview'
+import MDReactComponent from 'markdown-react-js'
 
 import 'brace/mode/python';
 import 'brace/theme/github';
@@ -14,15 +16,18 @@ function onChange(newValue) {
 export default class Home extends Component {
   render() {
     return (
-      <AceEditor
-        mode="python"
-        theme="github"
-        onChange={onChange}
-        name="ace_editor"
-        editorProps={
+      <div id="main">
+        <AceEditor
+          mode="python"
+          theme="github"
+          onChange={onChange}
+          name="ace_editor"
+          editorProps={
           { $blockScrolling: true }
         }
-      />
+        />
+        <MarkdownPreview mdtext="Some text with **emphasis!**"/>
+      </div>
     );
   }
 }
