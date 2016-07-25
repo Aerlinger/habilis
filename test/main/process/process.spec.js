030745b320612ccb683a4fdaac57384deda054ff
Anthony Erlinger <aerlinger@gmail.com>
2016-07-25 17:25:28 -0400

    WIP on testing child process IPC for python

--- test/main/process/process.spec.js
+++ test/main/process/process.spec.js
@@ -1,26 +1,190 @@
 import { expect } from 'chai';
 import { spawn } from 'child_process';
 
-xdescribe("Child process", () => {
+import { create, kill, getChildren } from '../../../lib/main/processes'
 
-  it("spawns a child process", (done)=> {
-    const ls = spawn('ls', ['-lh', '/usr']);
+describe.only("Kernel", function() {
 
-    ls.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+  it("spawns a child process", function(done) {
+    this.timeout(20000)
 
-      expect(data.toString()).to.have.string('X11')
+    const kernel_path = "./test/fixtures/kernel/start_kernel.py"
 
-      done()
+    let spawned_child = create(kernel_path)
+
+    spawned_child.stderr.on('error', function(data) {
+      console.log("STDERR RECEIVED: ", data.toString())
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
     });
 
-    ls.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
+    spawned_child.stderr.on('data', function(data) {
+      console.log("STDERR RECEIVED: ", data.toString())
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
     });
 
-    ls.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
+    spawned_child.on('end', (data) => {
+      console.log("END RECEIVED: ", data.toString())
+
+      expect(getChildren().length).to.eql(-1)
+
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
     });
+
+    spawned_child.on('token', (data) => {
+      console.log("ERROR RECEIVED: ", data.toString())
+
+      expect(getChildren().length).to.eql(-1)
+
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.stdout.on('error', (data) => {
+      console.log("ERROR RECEIVED: ", data.toString())
+
+      expect(getChildren().length).to.eql(-1)
+
+      
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.stdout.on('data', (data) => {
+      console.log("DATA RECEIVED: ", data)
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.on('connect', (data) => {
+      console.log("connection RECEIVED: ", data)
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.on('connection', (data) => {
+      console.log("connection RECEIVED: ", data)
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.on('disconnect', (data) => {
+      console.log("DISC RECEIVED: ", data)
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.on('message', (data) => {
+      console.log("MESSAGE RECEIVED: ", data)
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.on('data', (data) => {
+      console.log("DATA RECEIVED: ", data)
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    spawned_child.on('error', function(data) {
+      console.log("STDERR RECEIVED: ", data.toString())
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+    
+    spawned_child.on('close', (data) => {
+      console.log("CLOSE EVENT RECEIVED: ", data)
+
+      expect(getChildren().length).to.eql(-1)
+
+      kill(spawned_child).then(function({code, signal}) {
+        console.log("EXIT SIGNAL: ", signal)
+        console.log("EXIT CODE: ", code)
+
+        done()
+      })
+    });
+
+    
   });
 
 })
