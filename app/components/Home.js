987c12002c9c7b8eaa77deaa560fefb2d4746a81
Anthony Erlinger <aerlinger@gmail.com>
2016-07-30 00:56:25 -0400

    Use a CodeMirror editor instead of AceEditor

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,36 +1,52 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
-import styles from './Home.css';
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import styles from './Home.css'
 
-import AceEditor from 'react-ace';
+import Codemirror from 'react-codemirror'
 import MarkdownPreview from './MarkdownPreview'
 import MDReactComponent from 'markdown-react-js'
 import ReactGridLayout from 'react-grid-layout'
 
-import 'brace/mode/python';
-import 'brace/theme/github';
+import 'codemirror/mode/python/python'
 
-function onChange(newValue) {
-  console.log('ACE change: ', newValue);
-}
 
 export default class Home extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      code: "// Code"
+    }
+  }
+
+  onChange(newValue) {
+    console.log('ACE change: ', newValue);
+  }
+
+  updateCode(newCode) {
+    this.setState({
+      code: newCode
+    })
+  }
+
   render() {
+    let options = {
+      lineNumbers: true
+    }
+
     return (
       <div id="main">
         <ReactGridLayout className="layout" cols={12} rowHeight={30} width={1200}>
-          <div key="a" data-grid={{x: 0, y: 0, w: 6, h: 20}}>
-            <AceEditor
-              mode="python"
-              theme="github"
-              onChange={onChange}
-              name="ace_editor"
-              editorProps={{ $blockScrolling: true }}
-            />
+
+          <div key="a" data-grid={{x: 0, y: 0, w: 60, h: 20}}>
+            <Codemirror  onChange={this.updateCode.bind(this)} options={options} />
           </div>
+
           <div key="b" data-grid={{x: 6, y: 0, w: 6, h: 2}}>
             <MarkdownPreview text="Some text with **emphasis!**"/>
           </div>
+
         </ReactGridLayout>
       </div>
     );
