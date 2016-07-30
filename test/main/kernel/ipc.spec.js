5f30034f6c188d1df71275c1a525eade0cb77009
Anthony Erlinger <aerlinger@gmail.com>
2016-07-30 13:53:07 -0400

    Add tests for additional client actions

--- test/main/kernel/ipc.spec.js
+++ test/main/kernel/ipc.spec.js
@@ -4,8 +4,8 @@ import _ from 'lodash'
 
 import uuid from 'uuid'
 
-import { JupyterClient, getPythonScriptResults, clientMap } from '../../lib/main/kernel/client'
-import { asInternal } from '../../lib/main/utils/log'
+import { JupyterClient, getPythonScriptResults, clientMap } from '../../../lib/main/kernel/client'
+import { asInternal } from '../../../lib/main/utils/log'
 
 asInternal(__filename)
 let log = console.log
@@ -218,9 +218,100 @@ describe("IPC messaging", function() {
     })
   })
 
+  xit("gets a result of a block of code", function(done) {
+    client.getResult("a='b'; foo='bar'").then(function(result) {
+      // expect(result["status"]).to.eql("ok")
+
+      // expect(result).to.have.keys("status", "execution_count", "user_expressions", "payload")
+
+      done()
+    })
+  })
+
+  it("execute returns an error for erroneous code", function(done) {
+    client.execute("a = b; foo='bar'").then(function(result) {
+
+      console.log("BASIC EXEC", result)
+
+      expect(result).to.have.keys('status', 'ename', 'evalue', 'traceback', 'user_expressions')
+      /*
+      ename: 'NameError',
+        evalue: 'name \'b\' is not defined',
+        traceback: [Object],
+        execution_count: 1,
+        user_expressions: {},
+        engine_info: [Object],
+        payload: [] })
+        */
+
+      done()
+    })
+  })
+
+  it("performs basic execution", function(done) {
+    client.execute("a = 'b'; foo='bar'; 1 + 1").then(function(result) {
+
+      console.log("BASIC EXEC", result)
+
+      expect(result["status"]).to.eql("ok")
+
+      done()
+    })
+  })
+
   it("gets variables", function(done) {
     client.getVariables().then(function(result) {
-      // expect(result).to.eql({})
+      console.log("RES", result)
+      expect(result).to.have.keys("function", "Series", "list", "DataFrame", "other", "dict", "ndarray")
+
+      done()
+    })
+  })
+
+  it("gets autocomplete", function(done) {
+    client.getAutoComplete("pd.").then(function(result) {
+      console.log("RESULT", result)
+      expect(result['matches']).to.include("pd.algos")
+
+      done()
+    })
+  })
+
+  it("gets isComplete", function(done) {
+    client.isComplete("1 + ").then(function(result) {
+      expect(result).to.eql({ status: 'invalid' })
+
+      done()
+    })
+  })
+
+  it("gets isComplete for complete statement", function(done) {
+    client.isComplete("pd.DataFrame").then(function(result) {
+      expect(result).to.eql({ status: 'complete' })
+
+      done()
+    })
+  })
+
+  it("gets isComplete for complete statement", function(done) {
+    client.isComplete("1 + 1 == 2").then(function(result) {
+      expect(result).to.eql({ status: 'complete' })
+
+      done()
+    })
+  })
+
+  it("gets docSrings for builtin type", function(done) {
+    client.getDocStrings(["sys"]).then(function(result) {
+      expect(result["text"]).to.eql("[{\"text\": \"sys\", \"docstring\": \"no docstring provided\", \"dtype\": \"---\"}]\n")
+
+      done()
+    })
+  })
+
+  it("gets docStrings", function(done) {
+    client.getDocStrings(["pd"]).then(function(result) {
+      expect(result["text"]).to.eql("[{\"text\": \"pd\", \"docstring\": \"\\npandas - a powerful data analysis and manipulation library for Python\\n=====================================================================\\n\\nSee http://pandas.pydata.org/ for full documentation. Otherwise, see the\\ndocstrings of the various objects in the pandas namespace:\\n\\nSeries\\nDataFrame\\nPanel\\nIndex\\nDatetimeIndex\\nHDFStore\\nbdate_range\\ndate_range\\nread_csv\\nread_fwf\\nread_table\\nols\\n\", \"dtype\": \"---\"}]\n")
 
       done()
     })
