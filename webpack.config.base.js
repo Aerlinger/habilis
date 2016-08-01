14ad5f1194817abe675aa511a7f898b7bc191834
Anthony Erlinger <aerlinger@gmail.com>
2016-08-01 02:55:40 -0400

    Use Photon SCSS & update webpack loaders for SCSS & URL

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,32 +1,44 @@
 import path from 'path';
 
 export default {
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel-loader'],
-      exclude: /node_modules/
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader'
-    }, {
-        test: /\.(yml)|(yaml)$/,
+  module:    {
+    loaders: [
+      {
+        test:    /\.jsx?$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test:   /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test:   /\.(yml)|(yaml)$/,
         loader: 'yaml-loader'
+      },
+      {
+        test:    /\.scss$/,
+        loaders: ["style", "css?sourceMap", "sass?sourceMap"]
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
       }
     ]
   },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./lib/renderer")]
+  },
+  output:    {
+    path:          path.join(__dirname, 'dist'),
+    filename:      'bundle.js',
     libraryTarget: 'commonjs2'
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.json'],
+  resolve:   {
+    extensions:   ['', '.js', '.jsx', '.json'],
     packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
   },
-  plugins: [
-
-  ],
+  plugins:   [],
   externals: [
     // put your node 3rd party libraries which can't be built with webpack here
     // (mysql, mongodb, and so on..)
