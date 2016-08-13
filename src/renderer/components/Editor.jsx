531778072e393092b69c3312ae44c47098321a8e
Anthony Erlinger <aerlinger@gmail.com>
2016-08-13 17:44:52 -0400

    Simple Widget example with codemirror

--- src/renderer/components/Editor.jsx
+++ src/renderer/components/Editor.jsx
@@ -7,12 +7,16 @@ import CodeMirror from 'codemirror'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/python/python'
 
+import _ from 'lodash'
+
 import styles from './Editor.css'
 import * as editor_actions from '../actions/editor'
 
 class Editor extends Component {
   constructor() {
     super()
+
+    this.widgets = []
 
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
   }
@@ -49,6 +53,42 @@ class Editor extends Component {
     }
   }
 
+  updateHints() {
+    let editor = this.codeMirror
+    let widgets = this.widgets
+
+    editor.operation(function() {
+      for (var i = 0; i < widgets.length; ++i)
+        editor.removeLineWidget(widgets[i])
+
+      widgets.length = 0
+      // JSHINT(editor.getValue())
+
+      let errs = [{
+        line: 5,
+        reason: "TEST ERR"
+      }]
+
+      for (var i = 0; i < errs.length; ++i) {
+        var err = errs[i]
+        if (!err) continue
+
+        var msg = document.createElement("div")
+        var icon = msg.appendChild(document.createElement("span"))
+        icon.innerHTML = "!!"
+        icon.className = "lint-error-icon"
+        msg.appendChild(document.createTextNode(err.reason))
+        msg.className = "lint-error"
+        widgets.push(editor.addLineWidget(err.line - 1, msg, {coverGutter: false, noHScroll: true}))
+      }
+    })
+
+    var info = editor.getScrollInfo()
+    var after = editor.charCoords({line: editor.getCursor().line + 1, ch: 0}, "local").top
+    if (info.top + info.clientHeight < after)
+      editor.scrollTo(null, after - info.clientHeight + 3)
+  }
+
   componentDidMount() {
     let options = {
       foldGutter:      true,
@@ -57,7 +97,8 @@ class Editor extends Component {
       extraKeys:       {
         "Ctrl--": function(cm) {
           cm.foldCode(cm.getCursor())
-        }
+        },
+        extraKeys: {"Alt-F": "findPersistent"}
       },
       gutters:         ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
     }
@@ -67,10 +108,18 @@ class Editor extends Component {
     this.codeMirror = codeMirrorInstance.fromTextArea(this.refs.editor, options)
     this.codeMirror.on('change', this.codemirrorValueChanged.bind(this))
     this.codeMirror.setValue(this.props.defaultValue || this.props.value || '')
+
+    let waiting
+    this.codeMirror.on("change", () => {
+      clearTimeout(waiting)
+      waiting = setTimeout(this.updateHints.bind(this), 1000)
+    })
+
+    setTimeout(this.updateHints.bind(this), 1000)
   }
 
   componentWillReceiveProps() {
-    return debounce(function(nextProps) {
+    return _.debounce(function(nextProps) {
       if (this.codeMirror && nextProps.value !== undefined && this.codeMirror.getValue() != nextProps.value)
         this.codeMirror.setValue(nextProps.value)
 
