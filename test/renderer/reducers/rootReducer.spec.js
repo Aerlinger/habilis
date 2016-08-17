0a23dcfce357a79668b0014b962a0b5730798844
Anthony Erlinger <aerlinger@gmail.com>
2016-08-17 00:04:45 -0400

    Example state

--- test/renderer/reducers/rootReducer.spec.js
+++ test/renderer/reducers/rootReducer.spec.js
@@ -16,15 +16,53 @@ describe('Root reducer', () => {
 
     expect(resultingState).to.eql(
       {
+        "ENV": {
+          python_ver: "2.7.11",
+          modules: []
+        },
+        "kernel": [
+          version: "",
+          port: "",
+          pid: "",
+          status: "",
+          namespace: {}
+        ],
         "currentWorkingDirectory": ".",
         "routing":                 {
           "locationBeforeTransitions": null
         },
         "editor":                  {
+          "hasFocus": true,
           "value": "def some_function():\n  a = 5\ndef another_function():\n  return \"foo\"",
           "pos": [30, 13],
           "cells": [
-
+            {
+              startLine: "int",
+              endLine: "int",
+              documentation: "",
+              idx: 0,
+              dirty: false,
+              id: "1",
+              state: "idle",
+              lines: [
+                "def another_function():\n  return \"foo\"",
+              ],
+              sha: "abcd1234",
+              "output": {
+                id: "abcd1234",
+                type: "table",
+                value: "<table></table>"
+              },
+              "history": [
+                {
+                  from: { ch: 0, line: 0 },
+                  to { ch: 14, 1 },
+                  text: ["def some_function"],
+                  removed: ["def some_function"],
+                  origin: "setValue"
+                }
+              ]
+            }
           ],
           "history": [
 
